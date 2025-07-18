83b1bab9fdce04fbaee57a6b8ef47ef9629aa87c
Joanny Prignol <joannyprignol@gmail.com>
2025-07-18 17:16:27 +0200

    initial commit
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

---
+++ back/chat-poc/src/main/java/com/your_car_your_way/chat_poc/ChatPocApplication.java
@@ -0,0 +1,13 @@
+package com.your_car_your_way.chat_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatPocApplication.class, args);
+	}
+
+}
---
+++ back/chat-poc/src/test/java/com/your_car_your_way/chat_poc/ChatPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.your_car_your_way.chat_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
